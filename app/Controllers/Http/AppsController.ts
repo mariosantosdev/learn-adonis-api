fa946bae9dbe554a4ea7b5b9f74d1cf5a07c8ee2
Mario Santos <mariodev7@gmail.com>
2022-02-28 00:58:48 -0300

    create apps controller

---
+++ app/Controllers/Http/AppsController.ts
@@ -0,0 +1,32 @@
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { schema } from "@ioc:Adonis/Core/Validator";
+
+import AppModel from "App/Models/App";
+
+export default class AppsController {
+  private schema = schema.create({
+    name: schema.string.nullable(),
+    email: schema.string.nullable(),
+  });
+
+  public async create({ response, request }: HttpContextContract) {
+    const { name, email } = await request.validate({
+      schema: this.schema,
+      messages: {
+        required: "The field {{field}} is required to register an app.",
+        nullable: "The field {{field}} is required to register an app.",
+      },
+    });
+
+    if (!name || !email) return;
+
+    const emailAlreadyExist = await AppModel.findBy("email", email);
+
+    if (emailAlreadyExist)
+      return response.badRequest("This email already exist in athoter app.");
+
+    const app = await AppModel.create({ name, email });
+
+    return { app };
+  }
+}
